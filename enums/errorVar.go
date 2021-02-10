7c415a4d70cabbe2316fcad13669e050d4912738
bingbing <875307054@qq.com>
2021-02-10 12:49:20 +0800

    调整参与次数消耗提示

--- enums/errorVar.go
+++ enums/errorVar.go
@@ -38,7 +38,7 @@ var (
 	ActivityJoinLogUpdateFailErr	= errors.New("更新活动参与记录因为加入活动因为人数已满失败出错")
 	ActivityUpdateJoinNumFailErr	= errors.New("更新活动参与人数出错")
 	ActivityStatusErr				= errors.New("活动不是可参加状态")
-	ActivityJoinLimit				= errors.New("您今天参与的活动次数已经达到5次了，明天再来吧")
+	ActivityJoinLimit				= errors.New("您今天参与的活动次数已经达到3次了，明天再来吧")
 	ActivityJoinLimitShare			= errors.New("今天参与次数已用完，把活动分享给好友并点击广告可获得参与次数")
 )
 
